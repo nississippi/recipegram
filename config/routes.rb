82dc9f01ed0684538fe2f07ea1e7d1aaa6a9c56d
nississippi <108444184+nississippi@users.noreply.github.com>
2023-03-07 16:18:46 +0000

    recipesコントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'recipes/new'
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/edit'
   devise_for :users
   root to: "home#index"
   resources :users
