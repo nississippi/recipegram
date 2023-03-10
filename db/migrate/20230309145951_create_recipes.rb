c4b33f2fb8dc9c18b5f80cffe77ccabe6fa95e9b
nississippi <108444184+nississippi@users.noreply.github.com>
2023-03-10 14:48:59 +0000

    recipeモデル

--- db/migrate/20230309145951_create_recipes.rb
+++ db/migrate/20230309145951_create_recipes.rb
@@ -1,6 +1,10 @@
 class CreateRecipes < ActiveRecord::Migration[6.1]
   def change
     create_table :recipes do |t|
+      t.string :title, null: false
+      t.text :body, null: false
+      t.string :image_id, null: false
+      t.integer :user_id, null: false
 
       t.timestamps
     end
